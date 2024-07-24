20e030cba537f96e850271416d6d7fb79cb391bd
Deepak Bijalwan <deepakbijalwan@Deepaks-MacBook-Pro.local>
2024-07-24 19:43:29 +0530

    sample controller

---
+++ ms/src/main/java/com/concepts/ms/rest/Controller.java
@@ -0,0 +1,15 @@
+package com.concepts.ms.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
